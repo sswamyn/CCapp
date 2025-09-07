0de6662b6648789d9e9eb82cbf07dd33677a8a0b
Swamy Sivasubramaniyan <sswamyn@users.noreply.github.com>
2025-09-07 12:08:46 -0600

    Fresh SB

---
+++ src/main/java/com/usb/demo/ccapp/CCappApplication.java
@@ -0,0 +1,13 @@
+package com.usb.demo.ccapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CCappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CCappApplication.class, args);
+    }
+
+}
